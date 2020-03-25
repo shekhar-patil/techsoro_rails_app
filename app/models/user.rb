b3a04ae6c99db1cb94e7a4f6bbf1746e952d3cf4
shekhar-patil <patilshekhar900@gmail.com>
2020-03-25 18:59:32 +0530

    remove has_secure_password from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
-  has_secure_password
 end
